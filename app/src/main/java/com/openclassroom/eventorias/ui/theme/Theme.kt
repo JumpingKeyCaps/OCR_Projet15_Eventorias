28d8ad6e218a510059c89e61f70bdb1b213c3c8f
Samuel Lebailly <sam.meusa@gmail.com>
2024-12-17 16:53:10 +0100

    fix(ui/theme): Fix dynamic color activation on the app theme.

--- app/src/main/java/com/openclassroom/eventorias/ui/theme/Theme.kt
+++ app/src/main/java/com/openclassroom/eventorias/ui/theme/Theme.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -14,9 +14,9 @@ private val DarkColorScheme = darkColorScheme(
     secondary = PurpleGrey80,
     tertiary = Pink80,
     background = eventorias_black,
-    onBackground = eventorias_black,
+    onBackground = Color.White,
     surface = eventorias_black,
-    onSurface = eventorias_black
+    onSurface = eventorias_black,
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -43,13 +43,13 @@ private val LightColorScheme = lightColorScheme(
 fun EventoriasTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicDarkColorScheme(context)
         }
 
         //TODO - On force le darktheme pour le momement !
