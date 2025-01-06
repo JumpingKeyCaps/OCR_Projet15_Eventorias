1e01e644bcfe2936d271a045fb59f7e80ec4647b
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-06 22:10:05 +0100

    chore(domain): Clean and add documentation.

--- app/src/main/java/com/openclassroom/eventorias/domain/User.kt
+++ app/src/main/java/com/openclassroom/eventorias/domain/User.kt
@@ -3,33 +3,18 @@ package com.openclassroom.eventorias.domain
 import java.io.Serializable
 
 /**
- * This class represents a User data object. It holds basic information about a user, including
- * their ID, first name, and last name. The class implements Serializable to allow for potential
- * serialization needs.
+ * User data model.
+ * Represents an user with his various attributes.
+ * @property id the user unique identifier
+ * @property firstname the user firstname
+ * @property lastname the user lastname
+ * @property email the user email
+ * @property pictureURL the user picture URL
  */
 data class User(
-    /**
-     * Unique identifier for the User.
-     */
     val id: String = "",
-
-    /**
-     * User's first name.
-     */
     val firstname: String = "",
-
-    /**
-     * User's last name.
-     */
     val lastname: String = "",
-
-    /**
-     * User's email.
-     */
     val email: String = "",
-
-    /**
-     * User'S picture url.
-     */
     val pictureURL: String? = null
 ) : Serializable
