94d6896c9e6f4f5014fea5edffab7f98c91194b1
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-07 00:20:01 +0100

    feat(data/service/firestore):Reworking the FireStore service to add new communication functions with database for the events data.

--- app/src/main/java/com/openclassroom/eventorias/data/service/firestore/FirestoreService.kt
+++ app/src/main/java/com/openclassroom/eventorias/data/service/firestore/FirestoreService.kt
@@ -1,7 +1,18 @@
 package com.openclassroom.eventorias.data.service.firestore
 
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import com.openclassroom.eventorias.domain.Event
 import com.openclassroom.eventorias.domain.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 /**
@@ -11,81 +22,203 @@ class FirestoreService {
 
     private val db = FirebaseFirestore.getInstance("eventoriasdb")
 
-    // ---------- User stuff
+
+
+
+    //----------- User stuff with Suspend ---------------------
+
     /**
      * Ajoute un utilisateur à Firestore.
      * @param user L'utilisateur à ajouter.
-     * @param onSuccess La fonction à exécuter en cas de succès.
-     * @param onFailure La fonction à exécuter en cas d'échec.
      */
-    fun addUser(user: User, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+    suspend fun addUser(user: User) {
         db.collection("users")
-            .document(user.id)  // set user id
-            .set(user) // create user or updtate if already exist
-            .addOnSuccessListener {
-                onSuccess()
-            }
-            .addOnFailureListener { e ->
-                onFailure(e)
-            }
+            .document(user.id) // set user id
+            .set(user) // create user or update if already exist
+            .await()
     }
 
     /**
      * Récupère un utilisateur par son ID.
      * @param uid L'ID de l'utilisateur.
-     * @param onSuccess La fonction à exécuter en cas de succès.
-     * @param onFailure La fonction à exécuter en cas d'échec.
+     * @return L'utilisateur correspondant à l'ID.
+     * @throws Exception si l'utilisateur n'existe pas ou en cas d'erreur.
      */
-    fun getUserById(uid: String, onSuccess: (User) -> Unit, onFailure: (Exception) -> Unit) {
-        db.collection("users")
-            .document(uid)  // On spécifie l'ID du document (ici l'UID de l'utilisateur)
-            .get()  // On récupère le document
-            .addOnSuccessListener { documentSnapshot ->
-                if (documentSnapshot.exists()) {
-                    val user = documentSnapshot.toObject(User::class.java)  // Convertir le document en objet User
-                    if (user != null) {
-                        onSuccess(user)  // Retourne l'utilisateur via le callback onSuccess
-                    } else {
-                        onFailure(Exception("User data is null"))  // Gestion de l'erreur si l'utilisateur est null
-                    }
-                } else {
-                    onFailure(Exception("User not found"))  // L'utilisateur n'existe pas dans Firestore
-                }
-            }
-            .addOnFailureListener { e ->
-                onFailure(e)  // En cas d'erreur, retourne l'exception
-            }
+    suspend fun getUserById(uid: String): User {
+        val documentSnapshot = db.collection("users")
+            .document(uid)
+            .get()
+            .await()
+
+        if (!documentSnapshot.exists()) {
+            throw Exception("User not found")
+        }
+
+        return documentSnapshot.toObject<User>()
+            ?: throw Exception("Failed to convert document to User")
     }
-
-
-
-
-
-
-
-
-
 
     /**
      * Supprime un utilisateur par son ID.
      * @param uid L'ID de l'utilisateur.
-     * @param onSuccess La fonction à exécuter en cas de succès.
-     * @param onFailure La fonction à exécuter en cas d'échec.
      */
-    fun deleteUser(uid: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+    suspend fun deleteUser(uid: String) {
         db.collection("users")
-            .document(uid)  // Cible le document de l'utilisateur avec son UID
-            .delete()  // Supprime le document de l'utilisateur
-            .addOnSuccessListener {
-                onSuccess()  // Appelle onSuccess si la suppression réussit
-            }
-            .addOnFailureListener { e ->
-                onFailure(e)  // En cas d'échec, appelle onFailure avec l'exception
-            }
+            .document(uid)
+            .delete()
+            .await()
+    }
+
+    /**
+     * Met à jour l'URL de la photo de profil de l'utilisateur.
+     * @param uid L'ID de l'utilisateur à mettre à jour.
+     * @param profilePictureUrl La nouvelle URL de la photo de profil.
+     */
+    suspend fun updateUserProfilePicture(uid: String, profilePictureUrl: String) {
+        db.collection("users")
+            .document(uid)
+            .update("pictureURL", profilePictureUrl)
+            .await()
+    }
+
+    /**
+     * Récupère l'URL de la photo de profil de l'utilisateur.
+     * @param userId L'ID de l'utilisateur.
+     * @return L'URL de la photo de profil.
+     * @throws Exception si l'utilisateur ou l'URL n'existe pas.
+     */
+    suspend fun getUserProfilePicture(userId: String): String? {
+        val documentSnapshot = db.collection("users")
+            .document(userId)
+            .get()
+            .await()
+        return if (!documentSnapshot.exists()) {
+            null
+        }else{
+            documentSnapshot.getString("pictureURL")
+        }
     }
 
 
 
+
+    // ---------- Event stuff -------------------------
+
+
+    /**
+     * Ajoute ou met à jour un événement dans Firestore.
+     * @param event L'objet Event à ajouter ou mettre à jour.
+     */
+    suspend fun addEvent(event: Event) {
+        val eventId = event.id.ifBlank { db.collection("events").document().id }
+        db.collection("events")
+            .document(eventId)
+            .set(event.copy(id = eventId))
+            .await()
+    }
+
+
+    /**
+     * Récupère tous les événements depuis Firestore.
+     * @return Une liste d'objets Event.
+     */
+    fun fetchEventsAsFlow(): Flow<List<Event>> = callbackFlow {
+        val listenerRegistration = db.collection("events")
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    close(error) // Ferme le flow en cas d'erreur
+                    return@addSnapshotListener
+                }
+
+                if (snapshot != null) {
+                    val events = snapshot.toObjects(Event::class.java)
+                    val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+                    val eventsWithPicture = events.map { event ->
+                        async {
+                            val authorPictureUrl = getUserProfilePictureUrl(event.authorId)
+                            event.copy(authorPictureURL = authorPictureUrl)
+                        }
+                    }
+
+                    coroutineScope.launch {
+                        val fetchedEvents = mutableListOf<Event>()
+                        for (deferredEvent in eventsWithPicture) {
+                            fetchedEvents.add(deferredEvent.await())
+                        }
+                        trySend(fetchedEvents) // Emit final list
+                    }
+                } else {
+                    trySend(emptyList()) // Émet une liste vide si aucun événement n'est trouvé
+                }
+            }
+
+        // Assurez-vous de supprimer le listener lorsque le flow est annulé
+        awaitClose { listenerRegistration.remove() }
+    }
+
+    /**
+     * Récupère l'URL de la photo de profil de l'utilisateur.
+     * @param userId L'ID de l'utilisateur.
+     * @return L'URL de la photo de profil ou null si l'utilisateur n'a pas de photo de profil.
+     */
+    private suspend fun getUserProfilePictureUrl(userId: String): String? {
+        val documentSnapshot = db.collection("users")
+            .document(userId)
+            .get()
+            .await()
+        return documentSnapshot.getString("pictureURL")
+    }
+
+    /**
+     * Supprime un événement spécifique depuis Firestore.
+     * @param eventId L'ID de l'événement à supprimer.
+     */
+    suspend fun deleteEvent(eventId: String) {
+        db.collection("events")
+            .document(eventId)
+            .delete()
+            .await()
+    }
+
+    /**
+     * Récupère un événement spécifique par son ID.
+     * @param eventId L'ID de l'événement à récupérer.
+     * @return Un objet Event ou null si l'événement n'existe pas.
+     */
+    suspend fun getEventById(eventId: String): Event? {
+        val document = db.collection("events")
+            .document(eventId)
+            .get()
+            .await()
+
+        val event = document.toObject(Event::class.java)
+
+        return event?.let {
+            val authorPictureUrl = getUserProfilePictureUrl(it.authorId)
+            it.copy(authorPictureURL = authorPictureUrl)
+        }
+    }
+
+    /**
+     * Ajoute un participant à un événement.
+     * @param eventId L'ID de l'événement.
+     * @param userId L'ID de l'utilisateur à ajouter.
+     */
+    suspend fun addParticipant(eventId: String, userId: String) {
+        val eventRef = db.collection("events").document(eventId)
+        eventRef.update("participants", FieldValue.arrayUnion(userId)).await()
+    }
+
+    /**
+     * Supprime un participant d'un événement.
+     * @param eventId L'ID de l'événement.
+     * @param userId L'ID de l'utilisateur à retirer.
+     */
+    suspend fun removeParticipant(eventId: String, userId: String) {
+        val eventRef = db.collection("events").document(eventId)
+        eventRef.update("participants", FieldValue.arrayRemove(userId)).await()
+    }
 
 
 
