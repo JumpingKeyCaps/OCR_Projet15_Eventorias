0ded7bff8f4dc906c9da4bb8c4dd76fb6ec41f64
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-06 23:13:28 +0100

    feat(navigation): Update the navigation host.

--- app/src/main/java/com/openclassroom/eventorias/navigation/EventoriasNavHost.kt
+++ app/src/main/java/com/openclassroom/eventorias/navigation/EventoriasNavHost.kt
@@ -1,11 +1,17 @@
 package com.openclassroom.eventorias.navigation
 
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.openclassroom.eventorias.screen.auth.AuthenticationScreen
 import com.openclassroom.eventorias.screen.main.MainScreen
+import com.openclassroom.eventorias.screen.main.eventCreation.EventCreationScreen
+import com.openclassroom.eventorias.screen.main.eventDetails.EventDetailsScreen
 
 @Composable
 fun EventoriasNavHost(
@@ -20,7 +26,45 @@ fun EventoriasNavHost(
         //-- Main Screen
         composable(route = Screens.Main.route) {
            //TODO - App main Screen
-            MainScreen()
+            MainScreen(onLogOutAction = {
+                    navHostController.navigate(Screens.Authentication.route){
+                        //clean the nav backstack
+                        popUpTo(0)
+                        launchSingleTop = true
+                        restoreState = false
+                    }
+                },
+                onCreateEventClicked = {
+                    navHostController.navigate(Screens.CreateEvent.route){
+                        launchSingleTop = true
+                    }
+                },
+                onEventClicked = {
+                    navHostController.navigate(Screens.EventDetails.createRoute(it.id)){
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+
+        //-- Event Details Screen
+        composable(route = Screens.EventDetails.route,
+            arguments = Screens.EventDetails.navArguments,
+
+        ) { backStackEntry ->
+            //TODO - Event Details Screen
+            val eventId = backStackEntry.arguments?.getString("postId")
+                ?: throw IllegalStateException("Event ID is required")
+
+
+
+            EventDetailsScreen(
+                eventId = eventId,
+                onBackClicked = {
+                    navHostController.popBackStack()
+                }
+            )
         }
 
 
@@ -37,6 +81,24 @@ fun EventoriasNavHost(
                         restoreState = false
                     }
 
+                }
+            )
+        }
+
+
+        //-- Create event Screen
+        composable(route = Screens.CreateEvent.route,
+            enterTransition = {
+                slideInHorizontally(initialOffsetX = { fullWidth -> fullWidth }) + fadeIn()
+            },
+            exitTransition = {
+                slideOutHorizontally(targetOffsetX = { fullWidth -> fullWidth }) + fadeOut()
+            }
+            ) {
+            //TODO - Create event Screen
+            EventCreationScreen(
+                onBackClicked = {
+                    navHostController.popBackStack() // use the popBackStack() function to navigate back
                 }
             )
         }
