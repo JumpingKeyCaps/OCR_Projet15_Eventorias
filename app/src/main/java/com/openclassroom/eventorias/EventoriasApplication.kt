09018cabc6db1d47d09ea49ee194963f1193db2c
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-06 22:42:33 +0100

    feat(eventorias): Initialisation of the notification channel on the application.

--- app/src/main/java/com/openclassroom/eventorias/EventoriasApplication.kt
+++ app/src/main/java/com/openclassroom/eventorias/EventoriasApplication.kt
@@ -1,6 +1,9 @@
 package com.openclassroom.eventorias
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,9 +13,26 @@ import dagger.hilt.android.HiltAndroidApp
  */
 @HiltAndroidApp
 class EventoriasApplication : Application(){
+
+    /**
+     * onCreate method of the application.
+     *  - initializes Firebase.
+     *  - creates a notification channel for API 26+.
+     */
     override fun onCreate() {
         super.onCreate()
+        // Initialize Firebase
         FirebaseApp.initializeApp(this)
+        // Initialize notification channel (API 26+)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "default_channel",
+                "Default Channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
