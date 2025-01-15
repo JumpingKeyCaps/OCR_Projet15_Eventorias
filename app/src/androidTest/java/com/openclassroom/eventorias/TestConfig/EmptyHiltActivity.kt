784997cf25bd5f382260f3bde13a50eb98aaa496
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-15 16:06:43 +0100

    test(AndroidTest/TestConfig): Create test activity for compose elements tests.

---
+++ app/src/androidTest/java/com/openclassroom/eventorias/TestConfig/EmptyHiltActivity.kt
@@ -0,0 +1,7 @@
+package com.openclassroom.eventorias.TestConfig
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EmptyHiltActivity : ComponentActivity()
